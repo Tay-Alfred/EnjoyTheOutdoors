1b378c01dd45a35ddb3a61e679326377c30e4489
Taylor <4.alfred.taylor@gmail.com>
2024-05-22 20:40:39 -0400

    final commit for directory ppage

---
+++ scripts/fulldirectory.js
@@ -0,0 +1,15 @@
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  var button = document.getElementById("backToTopBtn");
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    button.style.display = "block";
+  } else {
+    button.style.display = "none";
+  }
+}
+
+function scrollToTop() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
